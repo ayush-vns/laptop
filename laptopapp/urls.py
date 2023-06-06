3bbabb073a9fd77c63e577304f9adab9df1c0104
ayush-vns <128778513+ayush-vns@users.noreply.github.com>
2023-06-06 14:30:37 +0530

    Removed Unnecessary files

--- laptopapp/urls.py
+++ laptopapp/urls.py
@@ -5,17 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.sbi),
     path("index", views.index),
-    path("create", views.create),
-    path("search", views.search),
-    path("update", views.update),
-    path("delete", views.delete),
-    path("all", views.Alllaptops),
-    path("price", views.Price),
-    path("between", views.between),
     path("account", views.account),
     path("deposit", views.deposit),
     path("withdraw", views.withdraw),
     path("details", views.details),
-    path("check", views.check)
+    
 
 ]
--- laptopapp/views.py
+++ laptopapp/views.py
@@ -1,122 +1,16 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .models import LaptopModel
 
 from .models import BankAccount
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Main")
+    return render(request, "sbi.html")
 
 
 def sbi(request):
     return render(request, "sbi.html")
 
-
-def create(request):
-    print("Create")
-    sno = ""
-    company_name = ""
-    Ram = ""
-    Rom = ""
-    Model_name = ""
-    price = ""
-    if request.GET:
-        sno = request.GET["sno"]
-        company_name = request.GET["Company_name"]
-        Ram = request.GET["Ram"]
-        price = request.GET["price"]
-        Rom = request.GET["Rom"]
-        Model_name = request.GET["Model_name"]
-        l = LaptopModel()
-        l.sno = sno
-        l.Company_name = company_name
-        l.Ram = Ram
-        l.Price = price
-        l.Rom = Rom
-        l.Model_name = Model_name
-        l.save()
-        print(sno, company_name, Ram, Rom, Model_name, price)
-
-    return render(request, "create.html",
-                  {"company_name": company_name, "Model_name": Model_name, "price": price, "sno": sno, "Ram": Ram,
-                   "Rom": Rom, })
-
-
-def search(request):
-    sno = ""
-    company_name = ""
-    Ram = ""
-    Rom = ""
-    Model_name = ""
-    price = ""
-    if request.GET:
-        price = request.GET["price"]
-        laptops = LaptopModel.objects.filter(Price=price)
-        n = len(laptops)
-        if n <= 0:
-            company_name = "not found"
-            Ram = "not found"
-            Rom = "not found"
-            Model_name = "not found"
-        else:
-            l = laptops[0]
-            company_name = l.Company_name
-            Model_name = l.Model_name
-            Ram = l.Ram
-            Rom = l.Rom
-        print(price)
-
-    return render(request, "search.html",
-                  {"company_name": company_name, "Model_name": Model_name, "price": price, "sno": sno, "Ram": Ram,
-                   "Rom": Rom, })
-
-
-def update(request):
-    return render(request, "update.html")
-
-
-def delete(request):
-    Company_name = ""
-    result = "Not deleted"
-    if request.GET:
-        Company_name = request.GET["company_name"]
-        laptops = LaptopModel.objects.filter(Company_name=Company_name)
-        n = len(laptops)
-        if n > 0:
-            Laptop = laptops[0]
-            Laptop.delete()
-            result = "Deleted"
-        # Book.delete(bookname)
-    return render(request, "delete.html", {"Company_name": Company_name, "result": result})
-
-
-def Alllaptops(request):
-    price
-    laptops = LaptopModel.objects.all()
-    print(laptops)
-    return render(request, "All laptops.html", {"laptops": laptops})
-
-
-def Price(request):
-    Price = ""
-    if request.GET:
-        price = request.GET["price"]
-        laptops = LaptopModel.objects.all().filter(Price=price)
-        # print(laptops, Price)
-    return render(request, "All laptops.html", {"laptops": laptops, "price": price})
-
-
-def between(request):
-    minprice = ""
-    maxprice = ""
-    laptops = LaptopModel.objects.all()
-    if request.GET:
-        minprice = request.GET["minprice"]
-        maxprice = request.GET["maxprice"]
-        laptops = LaptopModel.objects.all().filter(Price__gt=minprice) & LaptopModel.objects.all().filter(
-            Price__lt=maxprice)
-    return render(request, "between.html", {"laptops": laptops, "maxprice": maxprice, "minprice": minprice})
 
 
 def account(request):
